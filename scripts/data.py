915f96517d113f16d9ec8e0c71a7add7cdcb1fc5
Miquel India <mindia@verbio.com>
2023-06-08 15:55:19 +0200

    Replaced Librosa Feature Extractor with Torch Audio One

--- scripts/data.py
+++ scripts/data.py
@@ -1,56 +1,36 @@
-import pickle
 import numpy as np
-from random import randint, randrange
+from random import randint
 from torch.utils import data
-import soundfile as sf
-
-
-def featureReader(featurePath):
-    with open(featurePath, "rb") as pickleFile:
-        features = pickle.load(pickleFile)
-        return np.transpose(features)
-
-
-def normalizeFeatures(features, normalization="cmn"):
-    mean = np.mean(features, axis=0)
-    features -= mean
-    if normalization == "cmn":
-        return features
-    if normalization == "cmvn":
-        std = np.std(features, axis=0)
-        std = np.where(std > 0.01, std, 1.0)
-        return features / std
+import torchaudio
 
 
 class Dataset(data.Dataset):
-    def __init__(self, utterances, parameters):
+    def __init__(self, utterances, parameters, sampleRate=16000):
         "Initialization"
         self.utterances = utterances
         self.parameters = parameters
         self.num_samples = len(utterances)
-
-    def __normalize(self, features):
-        mean = np.mean(features, axis=0)
-        features -= mean
-        if self.parameters["normalization"] == "cmn":
-            return features
-        if self.parameters["normalization"] == "cmvn":
-            std = np.std(features, axis=0)
-            std = np.where(std > 0.01, std, 1.0)
-            return features / std
+        self.spectogram_extractor = torchaudio.transforms.MelSpectrogram(
+            n_fft=512,
+            win_length=int(sampleRate * 0.025),
+            hop_length=int(sampleRate * 0.01),
+            n_mels=80,
+            f_max=sampleRate // 2,
+            normalized=True,
+        )
 
     def __sampleSpectogramWindow(self, features):
-        file_size = features.shape[0]
+        file_size = features.size()[0]
         windowSizeInFrames = self.parameters["window_size"] * 100
         index = randint(0, max(0, file_size - windowSizeInFrames - 1))
         a = np.array(range(min(file_size, int(windowSizeInFrames)))) + index
         return features[a, :]
 
     def __getFeatureVector(self, utteranceName):
-        with open(utteranceName + ".pickle", "rb") as pickleFile:
-            features = pickle.load(pickleFile)
+        waveform, sample_rate = torchaudio.load(utteranceName + ".wav")
+        sample_spectogram = self.spectogram_extractor(waveform).squeeze(0)
         windowedFeatures = self.__sampleSpectogramWindow(
-            self.__normalize(np.transpose(features))
+            sample_spectogram.transpose(0, 1)
         )
         return windowedFeatures
 
