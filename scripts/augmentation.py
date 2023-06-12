e469dad123c4004e70d3487dafc7c1fc3e6e0564
Miquel India <mindia@verbio.com>
2023-06-12 18:32:07 +0200

    Add Speed Perturbation and Reverb

--- scripts/augmentation.py
+++ scripts/augmentation.py
@@ -1,10 +1,11 @@
 import random
 
+import torch
 import torchaudio
 
 
 class DataAugmentator:
-    EFFECTS = ["__apply_speed_perturbation", "__apply_reverb", "__add_background_noise"]
+    EFFECTS = ["apply_speed_perturbation", "apply_reverb", "add_background_noise"]
     SPEEDS = ["0.9", "1.1"]
     SNRS = ["10", "15", "20"]
 
@@ -16,15 +17,23 @@ class DataAugmentator:
         return len(self.augmentation_list)
 
     def __call__(self, audio, sample_rate):
-        return self.augment(audio)
+        return self.augment(audio, sample_rate)
 
-    def __apply_speed_perturbation(self, audio):
-        return audio
+    def apply_speed_perturbation(self, audio, sample_rate):
+        speed = random.choice(self.SPEEDS)
+        return torchaudio.sox_effects.apply_effects_tensor(
+            audio, sample_rate, [["speed", speed]]
+        )[0]
 
-    def __apply_reverb(self, audio):
-        return
+    def apply_reverb(self, audio, sample_rate):
+        return torch.mean(
+            torchaudio.sox_effects.apply_effects_tensor(
+                audio, sample_rate, [["reverb", "-w"]]
+            )[0],
+            dim=0,
+        ).unsqueeze(0)
 
-    def __add_background_noise(self, audio):
+    def add_background_noise(self, audio, sample_rate):
         return audio
 
     def __create_augmentation_list(self, augmentation_labels_path):
@@ -33,4 +42,4 @@ class DataAugmentator:
 
     def augment(self, audio, sample_rate):
         effect = random.choice(self.EFFECTS)
-        return getattr(self, effect)(audio)
+        return getattr(self, effect)(audio, sample_rate)
